5ddc60a81b6b068b062538a827ee8d91373ca932
pallat <yod.pallat@gmail.com>
2018-08-04 09:58:06 +0700

    show go routine

---
+++ sequence.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	t := time.Now()
+	one()
+	two()
+	three()
+	fmt.Println(t.Sub(time.Now()))
+}
+
+func one() {
+	time.Sleep(1 * time.Second)
+	fmt.Println("one")
+}
+
+func two() {
+	time.Sleep(2 * time.Second)
+	fmt.Println("two")
+}
+
+func three() {
+	time.Sleep(3 * time.Second)
+	fmt.Println("three")
+}
